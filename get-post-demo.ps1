cd88ccc914f5d0f52fcefca727e341ea5390e964
alanv73@gmail.com <alanv73@gmail.com>
2021-03-19 11:49:27 -0400

    add post and csv methods

--- get-post-demo.ps1
+++ get-post-demo.ps1
@@ -1,9 +1,9 @@
 using module .\Post.psm1
 
-$API_URL = "https://jsonplaceholder.typicode.com/posts"
+$API_BASE_URL = "https://jsonplaceholder.typicode.com"
 
 function GetFromAPI() {
-    $url = $API_URL
+    $url = "$API_BASE_URL/posts"
     $PostsArray = @()
     
     try {
@@ -13,7 +13,8 @@ function GetFromAPI() {
             $PostsArray += [Post]::fromJSON($post)
         }
 
-        $PostsArray | Out-GridView
+        Write-Host "$($PostsArray.count) Posts Loaded from API"
+        $PostsArray | Out-GridView -Title "Posts from API" -Wait
 
         return $PostsArray
     } catch {
@@ -22,28 +23,65 @@ function GetFromAPI() {
 }
 
 function PostToAPI($Object) {
-    Write-Host $Object
+    if ($null -eq $Object) {
+        Write-Host "Nothing Loaded"
+        return $null
+    }
+
+    $url = "$API_BASE_URL/posts"
+
+    $post = $Object | Out-GridView -Title "Select a Record to POST" -PassThru
+
+    if ($null -eq $post) {
+        Write-Host "No Record Selected"
+        return $null
+    }
+
+    $headers = New-Object System.Collections.Generic.Dictionary"[[String],[String]]"
+    $headers.add("Content-type", "application/json; charset=UTF-8")
+
+    $body = $post.toJSONString()
+
+    try {
+        $response = Invoke-RestMethod $url -Method Post -Headers $headers -Body $body
+        Write-Host "Record id: $($post.id) POSTed to API"
+        $response | Out-GridView -Title "Record POSTed" -Wait
+    } catch {
+        Write-Host "POST Error: $_"
+    }
+    
 }
 
 function SaveToCSV($Object) {
     $filename = Read-Host "Filename"
+    $Object | Export-CSV -Path $filename -NoTypeInformation
     Write-Host "$filename saved"
+    Invoke-Item $filename
 }
 
 function LoadFromCSV() {
     $filename = Read-Host "Filename"
+    $data = Import-CSV -Path $filename
+    $posts = @()
+
+    foreach ($datum in $data) {
+        $post = [Post]::fromJSON($datum)
+        $posts += $post
+    }
+
     Write-Host "Loaded $filename"
+    $posts | Out-GridView -Title "Posts Loaded from CSV file" -Wait
 }
 
 function MainMenu($Message) {
     Clear-Host
     Write-Host "==={ Main Menu }==="
-    Write-Host "Make a selection:"
-    Write-Host "1) GET Data from API"
+    Write-Host "`n1) GET Data from API"
     Write-Host "2) POST Data to API"
     Write-Host "3) Save Data to CSV"
     Write-Host "4) Load Data from CSV"
     Write-Host "Q) Quit"
+    Write-Host "`nMake a selection:"
     Write-Host "`n$Message"
     $Message = ""
 
